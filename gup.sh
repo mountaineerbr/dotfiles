9b04c5cdf570a2924751382703798aa9f65e5478
mountaineerbr <jsonineto@gmail.com>
2020-12-07 12:35:04 -0300

    sync

--- gup.sh
+++ gup.sh
@@ -3,41 +3,20 @@
 # v0.1  dec/2020  by mountaineerbr
 
 
-#copy different files to $PWD
-cpf()
-{
-	local f e r
-
-	for f in "${@#$PWD}"
-	do
-		e="$src/$f"
-		
-		if [[ -f "$e" ]] &&
-			! diff -q "$f" "$e" &>/dev/null
-		then
-			#set $C for actually copying files
-			((C)) || print "copy -- $e\nto   -- $PWD/$f" >&2
-			((C)) && cp -vf "$e" "$PWD/$f"
-		else
-			((C)) || print "skip -- $e" >&2
-		fi
-	done
-	
-	((C)) || print ">>>set \$C to non-empty to actually copy files" >&2
-}
-
 set -e
+
+#source diffcp()
+. ~/.rc
 
 setopt extendedglob globstarshort nullglob
 
 #~/ dotfiles
 #~/.config
-src="$HOME"  cpf  .*(.)  .config/**(.)
+src="$HOME"  diffcp  .*(.)  .config/**(.)
 
 #/etc
 cd etc
-src="/etc"  cpf  *(.)
+src="/etc"  diffcp  *(.)
 cp -f /etc/pacman.d/mirrorlist pacman.d/mirrorlist
 cd ..
-
 
