d978b8963a57ee6d6d47715616dd3f0b18630b15
jsonijr@gmail.com <netosj@yahoo.com>
2025-12-06 18:17:30 -0300

    sync

--- systemd/user/import_env.sh
+++ systemd/user/import_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #https://naftuli.wtf/2017/12/28/systemd-user-environment/
 #https://wiki.archlinux.org/title/Systemd/User#Environment_variables
+#https://github.com/i3/i3/issues/5186
 
 dbus-update-activation-environment --systemd --all ||
 	systemctl --user import-environment
